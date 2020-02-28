03987f3fd6af90a6a2868512e12f7c75da05b7e5
kipfer <christoph.kattmann@gmail.com>
2020-02-28 12:18:27 +0100

    Bugfixes for installation without Desktop

--- install.sh
+++ install.sh
@@ -8,12 +8,12 @@ then
     mv ~/.vimrc ~/.vimrc2
 fi
 cp $(pwd)/.vimrc ~/.vimrc
-sudo apt-get install -y vim-gnome
+sudo apt-get install -y vim
 sudo apt-get install -y build-essential cmake
 sudo apt-get install -y python-dev
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-python ~/.vim/bundle/YouCompleteMe/install.py
+python3 ~/.vim/bundle/YouCompleteMe/install.py
 chown -R $USER ~/.vim
  
 # Bash
